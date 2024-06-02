7801a37b92a02a5ba38dbab2f3aea9ddd4d96406
shivamtechinfo <168769050+shivamtechinfo@users.noreply.github.com>
2024-06-02 07:48:19 +0530

    Add files via upload

---
+++ cp16.c
@@ -0,0 +1,16 @@
+//Write a C program to swap two numbers using pointers.
+#include<stdio.h>
+int main() {
+    int num1, num2, temp;
+
+    printf("Enter two number:");
+    scanf("%d %d",  &num1, &num2);
+
+    printf("number %d and %d before swaping\n", num1, num2);
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    printf("number %d and %d after swaping", num1, num2);
+
+    return 0; 
+}
